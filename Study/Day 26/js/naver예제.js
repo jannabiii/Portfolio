f7df9bcaf5a7143b4722826447a67fb4b1981829
JANNABIII <ajonstae@Naver.com>
2019-11-14 15:37:29 +0900

    33일차 네이버 만들기

--- Study/Day 26/js/naver예제.js
+++ Study/Day 26/js/naver예제.js
@@ -1,3 +1,49 @@
-$(function(){
+$(document).ready(function(){
+    $('.keyboard').click(function(){
+        $('.keyboard-icon').toggleClass('green');
+    })
+})
+$(document).ready(function(){
+    $('.auto').click(function(){
+        $('.auto-icon').toggleClass('active');
+        $(".auto-box").toggle();
+    })
+})
+$(document).ready(function(){
+    $('.bar').click(function(){
+        $('.more').toggleClass('fold');
+    })
+})
+$(document).ready(function(){
+    $('.more').click(function(){
+        $('.fold').toggleClass('active');
+        $(".addbox").toggle();
+    })
+})
+$(document).ready(function(){
+    $('.more').click(function(){
+        $('.fold').toggleClass('active');
+        $(".addbox2").toggle();
+    })
+})
 
+$(document).ready(function(){
+    $('.button').click(function(){
+        var page = $('.page').text();
+        page = parseInt(page);
+        var max = $('.max').text();
+        max = parseInt(max);
+        if($(this).hasClass('prev')){
+            page += 1;
+            if(page > max){
+                page=1;
+            }
+            }else{
+                page -= 1;
+                if(page == 0){
+                page = max;
+            }
+        }
+        $('.page').text(page);
+    })
 })
