0b86c3f1971b4e6cb01f432ec07d2a14d4b4285f
JANNABIII <ajonstage@naver.com>
2019-11-28 15:59:08 +0900

    모바일 네이버 Day 33 각종 버튼효과

--- Study/Day 33/Js/mnaver.js
+++ Study/Day 33/Js/mnaver.js
@@ -47,4 +47,27 @@ $(document).ready(function(){
         $('.menu-list').css('background-image','linear-gradient(to right,#0eca68,#00bf5f');
     })
 })
-//네비게이션바(menu-list의 메뉴를 눌렀을시 해당위치로 하단 바 위치변경)
+//네비 스와이프 효과
+/*$(document).ready(function(){
+var tabMenu = new Swiper('.menu-list-container', {
+    centeredSlides:true,
+    centerdSlidesBounds:true,
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+    hashNavigation: true,
+});
+    var tabContents = new Swiper('.tabContents', {
+        spaceBetween: 10,
+        hashNavigation: true,
+        });
+})*/
+//드라마 예능 클릭(cell-menu)
+$(document).ready(function(){
+    $('.cell-list>li>a').click(function(){
+        $('.cell-list>li>a').removeClass('a');
+        $(this).addClass('a');
+    })
+})
+
