149ae2f9f8bd8504f45a4028086a668490f4efef
JANNABIII <ajonstage@naver.com>
2019-12-18 16:19:27 +0900

    샤카서프 페이지(메인+메뉴제작중)

--- Study/ShakaSURF/js/shaka.js
+++ Study/ShakaSURF/js/shaka.js
@@ -1,38 +1,73 @@
 $(function(){
-    var el = document.getElementsByClassName('moving');
-    for(var i=0; i<el.length; i++){
-        var deg = getDeg(el[i]);
-    }
+    
+    //--------------------------------------------------------스와이퍼
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+    });
+    //햄버거 메뉴 애니메이션 준 부분
+    document.querySelectorAll('.menu-css').forEach(btn => {
+        btn.addEventListener('click', e => {
+            btn.classList.toggle('active');
+        });
+    });
+    //---------------------------------------------------------------
+    var deg = 10;
+    var ms = 1000;
+    //
+    setInterval(function(){
+        $('.moving').each(function(){
+            var angle = calculateDeg(this);
+            //console.log(angle)
+            rotate(this, angle,deg,ms)
+            angle = calculateDeg(this);
+            rotate(this, angle,0,ms)
+            //console.log(angle)
+        })
+    },ms*Infinity)
 })
-function getDeg(el){
-    //var el = document.getElementById(selector);
-    var st = window.getComputedStyle(el, null);
-    var tr = st.getPropertyValue("-webkit-transform") ||
-            st.getPropertyValue("-moz-transform") ||
-            st.getPropertyValue("-ms-transform") ||
-            st.getPropertyValue("-o-transform") ||
-            st.getPropertyValue("transform") ||
-            "FAIL";
 
-    // With rotate(30deg)...
-    // matrix(0.866025, 0.5, -0.5, 0.866025, 0px, 0px)
-    console.log('Matrix: ' + tr);
-
-    // rotation matrix - http://en.wikipedia.org/wiki/Rotation_matrix
-
+//이미지 로테이션을 위한 함수 
+function calculateDeg(selector){
+    var tr = $(selector).css('transform');
     var values = tr.split('(')[1].split(')')[0].split(',');
     var a = values[0];
     var b = values[1];
     var c = values[2];
     var d = values[3];
-
     var scale = Math.sqrt(a*a + b*b);
-
     var sin = b/scale;
-    // next line works for 30deg but not 130deg (returns 50);
-    // var angle = Math.round(Math.asin(sin) * (180/Math.PI));
     var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-
-    return angle;
-
+    return angle
 }
+function rotate(selector, angle, deg, ms){
+    $(selector).animate({a: deg},{
+        duration:ms,
+        step: function(now,fx) {
+            $(this).css('transform','rotate('+(angle+now)+'deg)'); 
+        }
+    });
+}
+
+//이벤트 요일 클릭시 슬라이드로 애니메이션 되어 사진 내려오게하기
+$(document).ready(function(){
+    $('.event-date').click(function(){
+        $(this).siblings(".event-photo").slideToggle(500)
+    })
+})
+
+$(document).ready(function(){
+    $('.event-date').click(function(){
+        $(this).toggleClass("event-date2")
+    })
+})
+
+
+    
